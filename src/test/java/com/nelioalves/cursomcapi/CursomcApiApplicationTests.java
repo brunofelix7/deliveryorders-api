b6760843b0585347957f74d63e3145ed57d41284
Bruno Felix <bruno.felix10@hotmail.com>
2018-02-09 10:44:47 -0300

    projeto criado

---
+++ src/test/java/com/nelioalves/cursomcapi/CursomcApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nelioalves.cursomcapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CursomcApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
