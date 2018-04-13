afe85c81430de424121099de459694072be65699
bruno-synctech <bruno@synctech.com.br>
2018-04-13 10:04:31 -0300

    Criando operacao de instanciacao

--- src/main/java/com/nelioalves/cursomcapi/MyApiApplication.java
+++ src/main/java/com/nelioalves/cursomcapi/MyApiApplication.java
@@ -1,12 +1,28 @@
 package com.nelioalves.cursomcapi;
 
+import java.util.Arrays;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.nelioalves.cursomcapi.domain.Categoria;
+import com.nelioalves.cursomcapi.repositories.CategoriaRepository;
 
 @SpringBootApplication
-public class MyApiApplication {
-
+public class MyApiApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository repository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(MyApiApplication.class, args);
+	}
+
+	/*Executa alguma ação quando a aplicação iniciar*/
+	@Override
+	public void run(String... arg0) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		repository.save(Arrays.asList(cat1, cat2));
 	}
 }
