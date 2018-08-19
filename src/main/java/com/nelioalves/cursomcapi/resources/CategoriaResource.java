41be90741ae732404ae22164bedfc7fb53dbea2f
brunofelix7 <bruno.felix10@hotmail.com>
2018-08-19 13:44:37 -0300

    Inserindo uma nova Categoria com POST

--- src/main/java/com/nelioalves/cursomcapi/resources/CategoriaResource.java
+++ src/main/java/com/nelioalves/cursomcapi/resources/CategoriaResource.java
@@ -2,24 +2,35 @@ package com.nelioalves.cursomcapi.resources;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.nelioalves.cursomcapi.domain.Categoria;
 import com.nelioalves.cursomcapi.services.CategoriaService;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping(path = "/categorias")
 public class CategoriaResource {
 	
+	private final CategoriaService service;
+
 	@Autowired
-	private CategoriaService service;
-	
+	public CategoriaResource(CategoriaService service) {
+		this.service = service;
+	}
+
 	@GetMapping(path = "/{id}")
 	public ResponseEntity<?> find(@PathVariable("id") Integer id){
-		Categoria categoria = service.findById(id);
+		Categoria categoria = service.find(id);
 		return ResponseEntity.ok().body(categoria);
+	}
+
+	@PostMapping
+	public ResponseEntity<?> create(@RequestBody Categoria categoria){
+		categoria = service.save(categoria);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(categoria.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 
 }
--- src/main/java/com/nelioalves/cursomcapi/services/CategoriaService.java
+++ src/main/java/com/nelioalves/cursomcapi/services/CategoriaService.java
@@ -9,14 +9,23 @@ import com.nelioalves.cursomcapi.services.exceptions.ObjectNotFoundException;
 @Service
 public class CategoriaService {
 	
-	@Autowired
-	private CategoriaRepository repository;
+	private final CategoriaRepository repository;
 
-	public Categoria findById(Integer id){
+	@Autowired
+	public CategoriaService(CategoriaRepository repository) {
+		this.repository = repository;
+	}
+
+	public Categoria find(Integer id){
 		Categoria obj = repository.findOne(id);
 		if(obj == null) {
 			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName());
 		}
 		return obj;
+	}
+
+	public Categoria save(Categoria obj){
+		obj.setId(null);
+		return repository.save(obj);
 	}
 }
